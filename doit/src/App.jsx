fa97f87045449baf4906d0b804e19620a2a7ec79
CarlosSoft04 <carlostechsoftware@hotmail.com>
2024-05-26 13:02:05 -0300

    Modificacao: Explicando codigo

--- doit/src/App.jsx
+++ doit/src/App.jsx
@@ -51,22 +51,22 @@ function App() {
     setTodos(newTodos); //Atualiza o valor da constante, passando um novo array criado
   };
 
-  const removeTodo = (id) => {
-    const newTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(newTodos);
+  const removeTodo = (id) => { //Funcao passada como parametro um identifcador que sera removido.
+    const newTodos = todos.filter((todo) => todo.id !== id); //Cria um novo array usando o metodo filtrar e exclui a tarefa cujo 'id' corresponde ao id fornecido
+    setTodos(newTodos); //Atualiza o estado da variavel
   };
 
-  const completeTodo = (id) => {
-    const newTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+  const completeTodo = (id) => {  //Funcao passada como parametro um identifcador unico
+    const newTodos = todos.map((todo) => //Usa o map para criar um novo array de tarefa
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo //Para cada tarefa com o id correspondente, cria um novo objeto de tarefa com o campo isCompleted, alternado.Para as outras tarefas, mantem inalteradas.
     );
-    setTodos(newTodos);
+    setTodos(newTodos); //Atualiza o valor da constante.
   };
 
   return (
     <div className='app'>
       <h1>Lista de tarefas:</h1>
-      <Search search={search} setSearch={setSearch} />
+      <Search search={search} setSearch={setSearch} /> 
       <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className='todo-list'>
         {todos
